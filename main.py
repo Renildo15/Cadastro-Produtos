11a0d6e72c1a072f6737b9d9338d876bf65a4c2c
Renildo Rabi Vale Dos Santos <77634638+Renildo15@users.noreply.github.com>
2022-01-22 19:10:52 +0000

    Tela listar Dados

--- main.py
+++ main.py
@@ -1,21 +1,32 @@
 from PyQt5 import uic,QtWidgets
 import mysql.connector
 
+
+banco = mysql.connector.connect(
+	host = "localhost",
+	user = "root",
+	passwd = "",
+	database = "CADASTRO_PRODUTOS"
+)
+
 def main():
 	telaMenu.show()
 	telaCadastro.close()
+	listarDados.close()
 
-
-def op_Cadastrar():
+def tela_Cadastrar():
 	telaCadastro.show()
 	telaMenu.close()
 
+def op_Cadastrar():
 	codigo = telaCadastro.lineEdit.text()
 	nome = telaCadastro.lineEdit_2.text()
 	preco = telaCadastro.lineEdit_3.text()
 	quantidade = telaCadastro.lineEdit_4.text()
 	data_Val = telaCadastro.dateEdit.text()
 	data_cad = telaCadastro.dateEdit_2.text()
+
+	categoria = ""
 
 
 	print("Codigo:",codigo)
@@ -28,17 +39,48 @@ def op_Cadastrar():
 
 	if telaCadastro.radioButton.isChecked():
 		print("Categoria: Produtos de Limpeza")
+		categoria = "Produtos de Limpeza"
 	elif telaCadastro.radioButton_2.isChecked():
 		print("Categoria: Alimento")
+		categoria = "Alimento"
 	elif telaCadastro.radioButton_3.isChecked():
 		print("Categoria: Produtos Diversos")
+		categoria = "Produtos Diversos"
 	else:
 		print("Categoria: Não informado")
+		categoria = "Não informado"
 
-	
+
+	cursor = banco.cursor()
+	comando_SQL = "INSERT INTO PRODUTOS(CODIGO,NOME,PRECO,QUANTIDADE,DAT_VALIDADE,DAT_CADASTRO,CATEGORIA) VALUES(%s,%s,%s,%s,%s,%s,%s)"
+	dados = (str(codigo), str(nome), str(preco), str(quantidade), str(data_Val), str(data_cad), categoria)
+	cursor.execute(comando_SQL,dados)
+	banco.commit()
+
+	telaCadastro.lineEdit.setText("")
+	telaCadastro.lineEdit_2.setText("")
+	telaCadastro.lineEdit_3.setText("")
+	telaCadastro.lineEdit_4.setText("")
+
+	telaCadastro.radioButton.setCheckable(False)
+	telaCadastro.radioButton_2.setCheckable(False)
+	telaCadastro.radioButton_3.setCheckable(False)
 
 def op_Listar():
-	print("Listar")
+	listarDados.show()
+	telaMenu.close()
+
+	cursor = banco.cursor()
+	comando_SQL = "SELECT * FROM PRODUTOS"
+	cursor.execute(comando_SQL)
+	dados_lidos = cursor.fetchall()
+
+	listarDados.tableWidget.setRowCount(len(dados_lidos))
+	listarDados.tableWidget.setColumnCount(8)
+
+	for i in range(0, len(dados_lidos)):
+		for j in range(0, 8):
+			listarDados.tableWidget.setItem(i,j,QtWidgets.QTableWidgetItem(str(dados_lidos[i][j])))
 
 def op_Editar():
 	print("Editar")
@@ -52,10 +94,11 @@ def op_Imprimir():
 app = QtWidgets.QApplication([])
 telaMenu = uic.loadUi("telamenu.ui")
 telaCadastro = uic.loadUi("telaCadastro.ui")
+listarDados = uic.loadUi("listarDados.ui")
 
 
 #Menu principal
-telaMenu.pushButton_1.clicked.connect(op_Cadastrar)
+telaMenu.pushButton_1.clicked.connect(tela_Cadastrar)
 telaMenu.pushButton_2.clicked.connect(op_Listar)
 telaMenu.pushButton_3.clicked.connect(op_Editar)
 telaMenu.pushButton_4.clicked.connect(op_Excluir)
@@ -65,6 +108,10 @@ telaMenu.pushButton_5.clicked.connect(op_Imprimir)
 
 telaCadastro.pushButton_2.clicked.connect(main)
 telaCadastro.pushButton.clicked.connect(op_Cadastrar)
+
+#Tela Listar
+
+listarDados.pushButton_2.clicked.connect(main)
 
 
 telaMenu.show()
