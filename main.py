788a456054e2f249c7a8f46ffa858b1d7eeb7ffa
Renildo Rabi Vale Dos Santos <77634638+Renildo15@users.noreply.github.com>
2022-01-22 00:57:40 +0000

    Tela Cadastro

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from PyQt5 import uic,QtWidgets
 
+def main():
+	telaMenu.show()
+	telaCadastro.close()
+
 
 def op_Cadastrar():
-	print("Cadastrar")
+	telaCadastro.show()
+	telaMenu.close()
 
 def op_Listar():
 	print("Listar")
@@ -18,11 +23,19 @@ def op_Imprimir():
 
 app = QtWidgets.QApplication([])
 telaMenu = uic.loadUi("telamenu.ui")
+telaCadastro = uic.loadUi("telaCadastro.ui")
+
+
+#Menu principal
 telaMenu.pushButton.clicked.connect(op_Cadastrar)
 telaMenu.pushButton_2.clicked.connect(op_Listar)
 telaMenu.pushButton_3.clicked.connect(op_Editar)
 telaMenu.pushButton_4.clicked.connect(op_Excluir)
 telaMenu.pushButton_5.clicked.connect(op_Imprimir)
+
+#Tela de cadastro
+
+telaCadastro.pushButton_2.clicked.connect(main)
 
 telaMenu.show()
 app.exec()
